27ebc0e2727e5522607b14b28bb2af3bbaadaf5f
Dhananjay Sharma <sharmadhananjay42@gmail.com>
2020-05-20 20:30:30 +0530

    Initial spring boot code

---
+++ src/main/java/com/asyouwish/SpringBootReactCompleteApplication.java
@@ -0,0 +1,13 @@
+package com.asyouwish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactCompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactCompleteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asyouwish/SpringBootReactCompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asyouwish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactCompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
