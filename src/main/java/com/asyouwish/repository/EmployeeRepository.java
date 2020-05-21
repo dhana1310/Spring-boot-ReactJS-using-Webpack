f5097c9f9ced136dfbbe07b01cb6b11a511a172e
Dhananjay Sharma <sharmadhananjay42@gmail.com>
2020-05-21 14:48:25 +0530

    Spring boot and react code added, will be bundled using webpack in static/built folder and deployed on port 8080

---
+++ src/main/java/com/asyouwish/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.asyouwish.repository;
+
+import com.asyouwish.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
